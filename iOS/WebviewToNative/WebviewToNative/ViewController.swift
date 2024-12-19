b92297f6c9840bd5514f41b35e7a1bf9bf214cea
arimura <33759663+arimura@users.noreply.github.com>
2024-12-19 11:03:42 +0900

    Add fluctsdk

--- iOS/WebviewToNative/WebviewToNative/ViewController.swift
+++ iOS/WebviewToNative/WebviewToNative/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 @preconcurrency import WebKit
+import FluctSDK
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, FSSRewardedVideoDelegate {
     
     private var webView: WKWebView!
     
@@ -44,14 +45,53 @@ class ViewController: UIViewController, WKNavigationDelegate {
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         NSLog("url: \(navigationAction.request.url?.absoluteString ?? "")")
         
-        if navigationAction.request.url?.absoluteString.contains("google") == true {
+        if navigationAction.request.url?.absoluteString.contains("google") == true
+            || navigationAction.request.url?.absoluteString.contains("about:blank") == true {
             decisionHandler(.allow)
         } else {
             decisionHandler(.cancel)
             NSLog("navigation canceled")
+            
+            FSSRewardedVideo.shared.delegate = self
+            FSSRewardedVideo.shared.load(withGroupId: "1000172151", unitId: "1000275015")
         }
     }
+    
+    func rewardedVideoDidLoad(forGroupID groupId: String, unitId: String) {
+        print("rewarded video ad did load")
+        if FSSRewardedVideo.shared.hasAdAvailable(forGroupId: "1000172151", unitId: "1000275015") {
+            // 動画リワード広告の表示
+            FSSRewardedVideo.shared.presentAd(forGroupId: "1000172151", unitId: "1000275015", from: self)
+        }
+    }
+    
+    func rewardedVideoShouldReward(forGroupID groupId: String, unitId: String) {
+        print("should rewarded for app user")
+    }
+    
+    func rewardedVideoDidFailToLoad(forGroupId groupId: String, unitId: String, error: Error) {
+        // refs: error code list are FSSRewardedVideoError.h
+        print("rewarded video ad load failed. Because \(error)")
+    }
+    
+    func rewardedVideoWillAppear(forGroupId groupId: String, unitId: String) {
+        print("rewarded video ad will appear")
+    }
+    
+    func rewardedVideoDidAppear(forGroupId groupId: String, unitId: String) {
+        print("rewarded video ad did appear")
+    }
+    
+    func rewardedVideoWillDisappear(forGroupId groupId: String, unitId: String) {
+        print("rewarded video ad will disappear")
+    }
+    
+    func rewardedVideoDidDisappear(forGroupId groupId: String, unitId: String) {
+        print("rewarded video ad did disappear")
+    }
+    
+    func rewardedVideoDidFailToPlay(forGroupId groupId: String, unitId: String, error: Error) {
+        // refs: error code list are FSSRewardedVideoError.h
+        print("rewarded video ad play failed. Because \(error)")
+    }
 }
-
-
-
